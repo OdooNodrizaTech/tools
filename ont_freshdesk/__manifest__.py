7eb9f264109ce78b1bd36c331240770af61e05dc
Informatica Arelux <informatica@arelux.com>
2020-03-24 07:11:43 +0100

    fix version

--- ont_freshdesk/__manifest__.py
+++ ont_freshdesk/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Ont Freshdesk',
-    'version': "10.0.1.0.0,    
+    'version': '10.0.1.0.0',    
     'author': 'Odoo Nodriza Tech (ONT)',
     'website': 'https://nodrizatech.com/',
     'category': 'Tools',
