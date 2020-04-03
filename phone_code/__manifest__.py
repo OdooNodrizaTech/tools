812c12b4d8c344190265d04f52a6787c0ae45739
Informatica Arelux <informatica@arelux.com>
2020-04-03 09:16:39 +0200

    phone_code

---
+++ phone_code/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Phone Code',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'crm'],
+    'data': [
+        'views/res_partner.xml',        
+    ],    
+    'installable': True,
+    'auto_install': False,    
+}
