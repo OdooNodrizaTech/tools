d07bcef08d05af506ccbb6d66bd9794205bb2578
Informatica Arelux <informatica@arelux.com>
2020-04-03 10:31:03 +0200

    account_invoice_not_paid_auto_send_mail

---
+++ ont_automation_account_invoice_not_paid_auto_send_mail/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ont Automation Account Invoice Not Paid Auto Send Mail',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'account_invoice_not_paid_auto_send_mail', 'ont_automation_base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,    
+}
