ca894787cf919c39e006ba7ab609d6b188cd080f
Informatica Arelux <informatica@arelux.com>
2020-04-03 14:38:20 +0200

    nuevos
    ont_automation_shipping_expedition_send_mail_info
    ont_automation_shipping_expedition_send_sms_info

---
+++ ont_automation_shipping_expedition_send_sms_info/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ont Automation Shipping Expedition Send Sms Info',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'shipping_expedition_send_sms_info', 'ont_automation_base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,    
+}
