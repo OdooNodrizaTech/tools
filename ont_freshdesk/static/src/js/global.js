eb9e8b5a41850d935e0f52121ff8ba8ca9027bc0
victoralmau <victoralmau@gmail.com>
2020-07-14 08:43:41 +0200

    reestructuración

--- ont_freshdesk/static/src/js/global.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    setTimeout(
-      function() 
-      {
-            $(".o_main_navbar .o_menu_systray").append('<li class="o_mail_systray_item" title="Arelux Freshdesk"><a href="https://grupoarelux.freshdesk.com/support/tickets" id="arelux_freshdesk" target="_blank"><i class="fa fa-life-ring"></i></a></li>');
-      }, 1000);    
-});
