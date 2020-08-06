fcc968f9731fb9041fc40f38615f9896c93bb12e
victoralmau <victoralmau@gmail.com>
2020-08-06 07:26:58 +0200

    errors

--- cesce/__manifest__.py
+++ cesce/__manifest__.py
@@ -12,6 +12,9 @@
         "base",
         "sale",
         "account",
+        "account_payment_mode",  # https://github.com/OCA/bank-payment
+        "account_payment_partner",  # https://github.com/OCA/bank-payment
+        "account_payment_sale"  # https://github.com/OCA/bank-payment
         "partner_financial_risk"  # https://github.com/OdooNodrizaTech/financial_risk
     ],
     "external_dependencies": {
